023f7bca7d48e2206c371504608ef95cde29a297
Ken Pettit <kpettit1@users.sourceforge.net>
2008-12-31 06:49:15 +0000

    Make serial port log file rx/tx indication all CAPS.

--- src/periph.cpp
+++ src/periph.cpp
@@ -1696,7 +1696,7 @@ void T100_ComMon::SaveLog(void)
 				else
 					rxtx = CLE_RX;
 
-				sprintf(outLine, "%04d %s ", line++, rxtx == CLE_TX ? "tx" : "rx");
+				sprintf(outLine, "%04d %s ", line++, rxtx == CLE_TX ? "TX" : "RX");
 				cnt = 0;			// No bytes on this line yet
 			}
 
