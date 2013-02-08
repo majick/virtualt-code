9f8297b6b137b109242a13e8eb618810d546887a
Ken Pettit <kpettit1@users.sourceforge.net>
2013-02-08 00:05:57 +0000

    Removed a few commented out printfs.

--- src/m100emu.c
+++ src/m100emu.c
@@ -544,7 +544,6 @@ void get_emulation_path(char* emu, int model)
 		strcat(emu, "PC8300/");
 		break;
 	}
-	//printf("emu_path:%s\n",emu);
 }
 
 /*
@@ -557,7 +556,6 @@ get_rom_path:	This function returns the path of the ROM file for the
 void get_rom_path(char* file, int model)
 {
 	strcpy(file, path);			/* Copy VirtualT path */
-	//printf("Path:%s\n",path);
 	
 	switch (model)
 	{
@@ -583,7 +581,6 @@ void get_rom_path(char* file, int model)
 		strcat(file, "PC8300/PC8300rom.bin");
 		break;
 	}
-	//printf("Rom_path:%s\n",file);
 }
 
 /*
